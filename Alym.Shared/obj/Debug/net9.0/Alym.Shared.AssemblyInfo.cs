d511e4e9099787614e46bcb218afda08eb0f46ed
Lufx2ytaro <kunticha09@gmail.com>
2025-09-10 20:45:56 +0300

    ---

--- Alym.Shared/obj/Debug/net9.0/Alym.Shared.AssemblyInfo.cs
+++ Alym.Shared/obj/Debug/net9.0/Alym.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Alym.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e999bc0d37ed33380cf5b0878009955f0ce4b81")]
 [assembly: System.Reflection.AssemblyProductAttribute("Alym.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Alym.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
